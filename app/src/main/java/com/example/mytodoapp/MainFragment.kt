464e2af86e3ad3f45ca4125e60fb7f7536390902
Tejas <Tejas75O25@gmail.com>
2021-08-21 16:18:30 +0530

    Created the UI of the main fragment

---
+++ app/src/main/java/com/example/mytodoapp/MainFragment.kt
@@ -0,0 +1,35 @@
+package com.example.mytodoapp
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.mytodoapp.databinding.MainFragmentBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var viewModel: MainViewModel
+
+    private lateinit var binding: MainFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.main_fragment,
+            container,
+            false
+        )
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        return binding.root
+    }
+
+}
